8f9bbf81a6d2f096922b3a79c5d0917c1ba16bfa
xCall <carlosfaita99@gmail.com>
2022-12-29 10:46:41 -0300

    Api base com c#

---
+++ RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        Person Update(Person person);
+        List<Person> FindAll();
+        void Delete(long id);
+    }
+}
---
+++ RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,52 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations {
+    public class PersonServiceImplementation : IPersonService {
+        private volatile int count;
+
+        public Person Create(Person person) {
+            return person;
+        }
+
+        public void Delete(long id) {
+
+        }
+
+        public List<Person> FindAll() {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i <8; i++) {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i) {
+            return new Person {
+                Id = IncrementeAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Piumhi - Minas Gerais" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementeAndGet() {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id) {
+            return new Person {
+                Id = IncrementeAndGet(),
+                FirstName = "Carlos",
+                LastName = "Uberlandia",
+                Address = "Piumhi - Minas Gerais",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person) {
+            return person;
+        }
+    }
+}
